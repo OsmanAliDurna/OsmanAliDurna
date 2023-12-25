e5a20d43ee05757c38341e05514753d012fe2b75
Osman Ali Durna <o_durna@hotmail.com>
2023-12-25 21:03:57 +0100

    last part

--- Denemelik/Benimkiler/Derslerden/rc15-16_clarus-store/inclass/src/pages/Products.jsx
+++ Denemelik/Benimkiler/Derslerden/rc15-16_clarus-store/inclass/src/pages/Products.jsx
@@ -1,17 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchInput from "../components/SearchInput";
 import ProductCard from "../components/ProductCard";
+import axios from "axios";
+import Loading from "../components/Loading";
 
 const Products = () => {
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  console.log(search);
+
+  const getData = async () => {
+
+    setLoading(true);
+
+    try {
+      const { data } = await axios(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+      console.log(data);
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   getData();
+  // }, []);//!compoentnDidMount yani başlagıçta çalış birdaha çalışma
+
+  useEffect(() => {
+    getData();
+  }, [search]); //! search statei değiştikçe getData fonksiyonu çalışsın
+
   return (
-    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      <SearchInput />
+    <div className="container">
+
+      <SearchInput search={search} setSearch={setSearch} />
+
       <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
         All Products
       </h2>
-      <div>
-        <ProductCard/>
-      </div>
+
+      {loading ? (
+        <Loading />
+      ) : products.length ? (
+        <div className="card-div">
+          {products.map((item) => (
+            <div key={item.id}>
+              <ProductCard item={item}/>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <h2 className="text-center text-3xl text-red-600 mt-32">No Products</h2>
+      )}
+
     </div>
   );
 };
