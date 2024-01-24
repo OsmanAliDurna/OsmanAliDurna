c38e90fc3d3377a4c73c326aa8fdeb3d115e8ab7
Osman Ali Durna <o_durna@hotmail.com>
2024-01-24 20:58:27 +0100

    some types lerned

--- Denemelik/Benimkiler/Derslerden/S01 - Intro/output/app.js
+++ Denemelik/Benimkiler/Derslerden/S01 - Intro/output/app.js
@@ -1,4 +1,21 @@
 "use strict";
 console.log("first");
 const add = (a, b) => a + b;
+let arr = [1, 2, 3];
+arr.push(Number("a"));
 //Hallo
+function greet() {
+    console.log("Hello world");
+}
+function error(message) {
+    throw new Error(message);
+}
+let abc = 0;
+function checkNumber(n) {
+    if (typeof abc == "string") {
+        abc.split;
+    }
+    else {
+        console.log(abc.toFixed(2));
+    }
+}
--- Denemelik/Benimkiler/Derslerden/S01 - Intro/src/app.ts
+++ Denemelik/Benimkiler/Derslerden/S01 - Intro/src/app.ts
@@ -1,5 +1,29 @@
 console.log("first");
 
-const add = (a:number, b:number) => a+b;
+const add = (a: number, b: number) => a + b;
+
+let arr: number[] = [1, 2, 3]
+
+arr.push(Number("a"))
 
 //Hallo
+
+function greet(): void {
+    console.log("Hello world");
+}
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+
+let abc: number | string = 0;
+
+function checkNumber(n: string | number): void {
+
+    if (typeof abc == "string") {
+        abc.split
+    } else {
+        console.log(abc.toFixed(2))
+    }
+}
