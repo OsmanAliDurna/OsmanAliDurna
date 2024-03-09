bacf9d8e742739403a9af83d0d519dcee3d72fa2
Osman Ali Durna <o_durna@hotmail.com>
2024-03-09 09:05:25 +0100

    Tekrar dersi

--- Udemy/React/React-Basics/rolodex/src/App.js
+++ Udemy/React/React-Basics/rolodex/src/App.js
@@ -1,4 +1,6 @@
-import { Component } from "react";
+import {
+  Component
+} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,38 +9,53 @@ class App extends Component {
     super();
 
     this.state = {
-      name: { firstName: "Osman", secondName: "Ali" },
-      company: "Anywhere",
+      // name: { firstName: "Osman", secondName: "Ali" },
+      // company: "Anywhere",
+      monsters: [{
+          name: "A",
+        },
+        {
+          name: "L",
+        },
+        {
+          name: "i",
+        }
+
+      ]
     };
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName} {this.state.name.secondName}, I just
-            want to work at {this.state.company}
-          </p>
-          <button
-            onClick={() => {
-              // this.setState({ name: "Anyone" });
-              this.setState(
-                (state, props) => {
-                  return {
-                    name: { firstName: "Any", secondName: "One" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            Change Name
-          </button>
-        </header>
+    return ( <
+      div className = "App" > {
+        /* <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Hi {this.state.name.firstName} {this.state.name.secondName}, I just
+                    want to work at {this.state.company}
+                  </p>
+                  <button
+                    onClick={() => {
+                      // this.setState({ name: "Anyone" });
+                      this.setState(
+                        (state, props) => {
+                          return {
+                            name: { firstName: "Any", secondName: "One" },
+                          };
+                        },
+                        () => {
+                          console.log(this.state);
+                        }
+                      );
+                    }}
+                  >
+                    Change Name
+                  </button>
+                </header> */
+      this.state.monsters.map((monster) => {
+        return <h1>{monster.name}</h1>;
+      })
+              } 
       </div>
     );
   }
